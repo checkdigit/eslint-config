7ce36c421f003e406899a2e3e4f080ce7939f05d
Sam Elliott De La Torre Babá <sdelatorrebaba@gmail.com>
2020-04-24 15:52:13 -0400

    ESLINT-32 add import/no-exteneous-dependencies (#31)
    * ESLINT-32 add import/no-exteneous-dependencies
    
    * ESLINT-32 add .test.ts files to devDependencies glob

--- index.js
+++ index.js
@@ -78,6 +78,7 @@ module.exports = {
     'import/order': ['error', {
       'newlines-between': 'ignore'
     }],
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/*.spec.ts', '**/*.test.ts'], 'optionalDependencies': false, 'peerDependencies': false}],
 
     'import/no-deprecated': 2,
 
