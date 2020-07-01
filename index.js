8032cf9733336ac6416961fa89ae9ea8a3862c75
Sam Elliott De La Torre Babá <sdelatorrebaba@gmail.com>
2020-07-01 13:50:34 -0400

    ESLINT-39 no-useless-constructor (#38)
    * ESLINT-39 no-useless-constructor
    
    * ESLINT-39 change to use error and off
    
    * ESLINT-39 remove comment
    
    * ESLINT-39 add testing on prepublish
    
    * ESLINT-39 remove beta tag
    
    * ESLINT-39 remove .vscode
    
    * ESLINT-39 use standard tsconfig.json

--- index.js
+++ index.js
@@ -21,11 +21,10 @@ module.exports = {
     'prettier/@typescript-eslint'
   ],
   rules: {
-    // 0 - off
-    // 1 - warn
-    // 2 - error
+    'no-useless-constructor': 'off',
+    '@typescript-eslint/no-useless-constructor': ['error'],
 
-    'func-names': 0,
+    'func-names': 'off',
 
     '@checkdigit/no-card-numbers': 'error',
     'no-secrets/no-secrets': ['error', { 'tolerance': 4.2 }],
@@ -33,44 +32,44 @@ module.exports = {
     'file-path-comment/file-comment-header': 'error',
     // Per require-await docs:
     // If you are throwing an error inside of an asynchronous function for this purpose, then you may want to disable this rule.
-    'require-await': 0,
-    '@typescript-eslint/require-await': 0,
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'off',
 
     // sonarjs doesn't implement the no-big-function rule probably because this rule already exists within stock eslint
     // set to 200 because that is sonar's default
     'max-lines-per-function': ['error', 200],
 
     // enforce use of curly braces around if statements and discourage one-line ifs
-    'curly': 2,
+    'curly': 'error',
 
     // always use ===
-    'eqeqeq': 2,
+    'eqeqeq': 'error',
 
     // undefined can be overwritten by assigning it to a variable (let undefined = 1)
-    'no-undefined': 2,
+    'no-undefined': 'error',
 
     // tslint does this and it feels like a good idea
-    'guard-for-in': 2,
+    'guard-for-in': 'error',
 
     // we probably shouldn't be using eval
-    'no-eval': 2,
-    'no-implied-eval': 2,
+    'no-eval': 'error',
+    'no-implied-eval': 'error',
 
     // disallow this keywords outside of classes or class-like objects. carl's gonna like this one
-    'no-invalid-this': 2,
+    'no-invalid-this': 'error',
 
     // sync methods are slow and we shouldn't be using them in production code
-    'no-sync': 2,
+    'no-sync': 'error',
 
     // modules we don't like
-    'no-restricted-modules': [2, 'url'],
+    'no-restricted-modules': ['error', 'url'],
 
     // prefer else-if
-    'no-lonely-if': 2,
+    'no-lonely-if': 'error',
 
     // make eslint feel just like the tslint days
-    'no-unneeded-ternary': 2,
-    'one-var': [2, 'never'],
+    'no-unneeded-ternary': 'error',
+    'one-var': ['error', 'never'],
 
     // these two configurations need to be kept in sync
     'sort-imports': ['error', {
@@ -82,62 +81,62 @@ module.exports = {
     }],
     'import/no-extraneous-dependencies': ['error', {'devDependencies': ['**/*.spec.ts', '**/*.test.ts'], 'optionalDependencies': false, 'peerDependencies': false}],
 
-    'import/no-deprecated': 2,
+    'import/no-deprecated': 'error',
 
-    'space-before-blocks': 2,
-    'space-unary-ops': 2,
-    'spaced-comment': 2,
-    'no-var': 2,
-    'prefer-const': 2,
-    'prefer-rest-params': 1,
-    'prefer-spread': 1,
-    'require-yield': 2,
-    'no-console': 2,
-    'no-return-await': 2,
+    'space-before-blocks': 'error',
+    'space-unary-ops': 'error',
+    'spaced-comment': 'error',
+    'no-var': 'error',
+    'prefer-const': 'error',
+    'prefer-rest-params': 'warn',
+    'prefer-spread': 'warn',
+    'require-yield': 'error',
+    'no-console': 'error',
+    'no-return-await': 'error',
 
     // having to manually type out the return type of all functions can get noisy
     // typescript does a good job at inferring types and detecting downstream errors
-    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-function-return-type': 'off',
 
-    '@typescript-eslint/ban-ts-comment': 2,
-    '@typescript-eslint/no-floating-promises': 2,
-    '@typescript-eslint/no-require-imports': 2,
+    '@typescript-eslint/ban-ts-comment': 'error',
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-require-imports': 'error',
 
     // sometimes fails on valid interface names like ISO8583
-    '@typescript-eslint/interface-name-prefix': 0,
+    '@typescript-eslint/interface-name-prefix': 'off',
 
     // typeof any === 'evil'.
-    '@typescript-eslint/no-explicit-any': 2,
+    '@typescript-eslint/no-explicit-any': 'error',
 
     // We're smarter than the default (15). Right?
     'sonarjs/cognitive-complexity': ['error', 24],
 
-    'no-only-tests/no-only-tests': 2,
+    'no-only-tests/no-only-tests': 'error',
 
     // eslint:all rules to modify
-    'sort-keys': 0,
-    'capitalized-comments': 0,
+    'sort-keys': 'off',
+    'capitalized-comments': 'off',
     'func-style': ['error', 'declaration', { 'allowArrowFunctions': true }],
-    'no-negated-condition': 0,
-    'multiline-comment-style': 0,
+    'no-negated-condition': 'off',
+    'multiline-comment-style': 'off',
     'no-magic-numbers': ['error', {'ignore': [-1, 0, 1, 2, 10, 16, 60]}],
-    'no-ternary': 0,
-    'max-params': [2, 8],
-    'max-statements': 0,
-    'consistent-return': 0,
-    'no-undef': 0,
-    'init-declarations': 0,
-    'no-inline-comments': 0,
-    'line-comment-position': 0,
-    'prefer-destructuring': 0,
-    'no-useless-return': 0,
-    'complexity': 0,
+    'no-ternary': 'off',
+    'max-params': ['error', 8],
+    'max-statements': 'off',
+    'consistent-return': 'off',
+    'no-undef': 'off',
+    'init-declarations': 'off',
+    'no-inline-comments': 'off',
+    'line-comment-position': 'off',
+    'prefer-destructuring': 'off',
+    'no-useless-return': 'off',
+    'complexity': 'off',
     'max-lines': ['error', {'max': 500, 'skipBlankLines': true, 'skipComments': true}],
     'id-length': ['error', {properties: 'never', exceptions: ['_']}],
     // as long as we don't rely on ASI this can remain off
-    'no-plusplus': 0,
-    'default-case': 0,
-    'no-continue': 0,
+    'no-plusplus': 'off',
+    'default-case': 'off',
+    'no-continue': 'off',
     'callback-return': ['error', ['callback', 'cb']],
     'new-cap': ['error', { 'capIsNew': false}]
   },
@@ -145,22 +144,28 @@ module.exports = {
     {
       files: ['*.spec.ts', '*.test.ts'],
       rules: {
-        'no-magic-numbers': 0,
-        'no-undefined': 0,
-        'max-lines-per-function': 0,
+        'no-magic-numbers': 'off',
+        'no-undefined': 'off',
+        'max-lines-per-function': 'off',
 
-        'sonarjs/no-duplicate-string': 0,
-        'sonarjs/no-identical-functions': 0,
-        'sonarjs/cognitive-complexity': 0,
+        'sonarjs/no-duplicate-string': 'off',
+        'sonarjs/no-identical-functions': 'off',
+        'sonarjs/cognitive-complexity': 'off',
 
-        'max-lines': 0,
-        'no-await-in-loop': 0
+        'max-lines': 'off',
+        'no-await-in-loop': 'off',
+
+        "@typescript-eslint/no-unsafe-call": "off",
+        "@typescript-eslint/no-unsafe-member-access": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/no-unsafe-return": "off",
+        "@typescript-eslint/restrict-template-expressions": "off"
       }
     },
     {
       files: ['swagger.ts'],
       rules: {
-        'max-lines': 0
+        'max-lines': 'off'
       }
     }
   ]
