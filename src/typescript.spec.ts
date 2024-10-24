675ba0896e782116ec5ce9105f7cbbc869b5a1f2
jagan <jagan.polavarapu@checkdigit.com>
2024-10-24 09:55:01 -0500

    Updated eslint-plugin-sonarjs

--- src/typescript.spec.ts
+++ src/typescript.spec.ts
@@ -1,10 +1,20 @@
 // typescript.spec.ts
 
+import { strict as assert } from 'node:assert';
+import { describe, it } from '@jest/globals';
 // it's ok to import test files from test code
 import testHello from './typescript.test';
 testHello();
 
 // linting error is not reported because unicorn/no-anonymous-default-export is disabled
+// eslint-disable-next-line jest/no-export
 export default function (): string {
   return 'hello';
 }
+
+describe('Sample Test Suite', () => {
+  it('should return hello', () => {
+    const result = testHello();
+    assert.equal(result, 'hello');
+  });
+});
--- src/typescript.ts
+++ src/typescript.ts
@@ -29,7 +29,7 @@ function hello(_?: string): bigint {
 
 // eslint-disable-next-line unicorn/no-array-for-each,unicorn/prevent-abbreviations
 ['', undefined].forEach((_lib) => {
-  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing,@typescript-eslint/strict-boolean-expressions
+  // eslint-disable-next-line @typescript-eslint/prefer-nullish-coalescing,@typescript-eslint/strict-boolean-expressions,sonarjs/prefer-nullish-coalescing
   const thing = _lib || {};
   // this is a comment for the regex below
   assert.ok(/^[a-z]+$/u.test('hello'));
@@ -41,7 +41,7 @@ function hello(_?: string): bigint {
 // uuid: 'c73bcdcc-2669-4bf6-81d3-e4ae73fb11fd' <- not ok since this is a non-test file
 hello();
 
-// eslint-disable-next-line require-yield
+// eslint-disable-next-line require-yield,  sonarjs/generator-without-yield
 async function* paginator() {
   throw new Error('should not be called');
 }
@@ -59,24 +59,25 @@ eval('console.log("no-no");');
 // eslint-disable-next-line no-invalid-this,@typescript-eslint/strict-boolean-expressions
 assert.ok(this);
 
+// eslint-disable-next-line sonarjs/pseudo-random
 if (Math.random()) {
   // ...
 } else {
-  // eslint-disable-next-line no-lonely-if
+  // eslint-disable-next-line sonarjs/no-lonely-if, sonarjs/pseudo-random, no-lonely-if
   if (Math.random()) {
     // ...
   }
 }
 
-// eslint-disable-next-line no-unneeded-ternary
-assert.ok(Math.random() === 2 ? true : false);
+// eslint-disable-next-line sonarjs/pseudo-random
+assert.ok(Math.random() === 2);
 
 // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions, @typescript-eslint/no-unnecessary-type-parameters
 const foo = <T>(argument: T) => (argument ? 1 : 0);
 // eslint-disable-next-line @typescript-eslint/strict-boolean-expressions
 assert.ok(foo);
 
-// eslint-disable-next-line @typescript-eslint/no-base-to-string
+// eslint-disable-next-line @typescript-eslint/no-base-to-string, sonarjs/no-base-to-string
 assert.ok({}.toString());
 
 export default hello;
@@ -87,7 +88,7 @@ const objectValue = { key: 'value' };
 // linting error is not reported because @typescript-eslint/restrict-template-expressions is configured to allow number and boolean
 assert(`I'm a number, ${numberValue}`);
 assert(`I'm a boolean, ${booleanValue}`);
-// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
+// eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string, sonarjs/no-base-to-string
 assert(`I'm a object, ${objectValue}`);
 
 // linting error is not reported because n/no-unsupported-features/node-builtins is disabled
