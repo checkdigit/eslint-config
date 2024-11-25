278473d90a5259a12d8177dd6233d8a501f407ba
jagan <jagan.polavarapu@checkdigit.com>
2024-11-25 11:37:13 -0600

    Changes to test new rule to detect assertions where statusCode is in the parameter

--- index.mjs
+++ index.mjs
@@ -369,6 +369,7 @@ const tsConfigurations = [
     files: ['**/*.spec.ts', '**/*.test.ts'],
     rules: {
       '@checkdigit/no-random-v4-uuid': 'off',
+      '@checkdigit/no-status-code-assertion': 'off',
       '@checkdigit/no-uuid': 'off',
       '@checkdigit/no-test-import': 'off',
       '@checkdigit/no-serve-runtime': 'error',
