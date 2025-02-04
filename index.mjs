f42f9456f0f6769b237db3791c8995deda226615
Le Cong <le.cong@checkdigit.com>
2025-02-04 17:45:59 -0500

    Disable no-status-code-assert rule in TypeScript configurations

--- index.mjs
+++ index.mjs
@@ -381,6 +381,7 @@ const tsConfigurations = [
       '@checkdigit/no-serve-runtime': 'error',
       '@checkdigit/no-side-effects': 'off',
       '@checkdigit/require-service-call-response-declaration': 'off',
+      '@checkdigit/no-status-code-assert': 'off',
       '@typescript-eslint/no-base-to-string': 'off',
       '@typescript-eslint/no-unsafe-argument': 'off',
       '@typescript-eslint/no-unsafe-call': 'off',
