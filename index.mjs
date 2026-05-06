24fb7019daaebf5ee4270d129fd52a6df1ea494b
Le Cong <le.cong@checkdigit.com>
2026-05-06 15:57:59 -0400

    prettier

--- index.mjs
+++ index.mjs
@@ -201,7 +201,7 @@ const tsConfigurations = [
       '@typescript-eslint/prefer-regexp-exec': 'off', // RegExp.exec() is *only* slightly faster than String.match() so will leave this off and not worth updating our code
 
       '@typescript-eslint/no-unsafe-enum-comparison': 'off', // With TypeScript v6, this rule behaves differently and starts reporting errors when a number is compared with an enum, e.g. `if (statusCode === StatusCodes.OK) { ... }`, which is a common pattern in our codebase.
-      
+
       // We're smarter than the default (15). Right?
       'sonarjs/cognitive-complexity': ['error', 24],
       'sonarjs/different-types-comparison': 'off', // duplicate of @typescript-eslint/no-unnecessary-condition
